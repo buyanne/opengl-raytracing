3eb99dab4bc181ad5b35bef2d96bd2f4fe0f9865
buyanne <1903683577@qq.com>
2022-04-20 19:53:13 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/gl.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/CandC++/C_Code/gl/main.cpp" "D:/CandC++/C_Code/gl/cmake-build-debug/CMakeFiles/gl.dir/main.cpp.obj"
+  "D:/CandC++/C_Code/gl/src/entity/Vector3d.cpp" "D:/CandC++/C_Code/gl/cmake-build-debug/CMakeFiles/gl.dir/src/entity/Vector3d.cpp.obj"
+  "D:/CandC++/C_Code/gl/src/entity/plane.cpp" "D:/CandC++/C_Code/gl/cmake-build-debug/CMakeFiles/gl.dir/src/entity/plane.cpp.obj"
+  "D:/CandC++/C_Code/gl/src/entity/sphere.cpp" "D:/CandC++/C_Code/gl/cmake-build-debug/CMakeFiles/gl.dir/src/entity/sphere.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/gl.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gl.dir/main.cpp.obj"
+  "CMakeFiles/gl.dir/src/entity/Vector3d.cpp.obj"
+  "CMakeFiles/gl.dir/src/entity/plane.cpp.obj"
+  "CMakeFiles/gl.dir/src/entity/sphere.cpp.obj"
+  "bin/gl.exe"
+  "bin/gl.exe.manifest"
+  "bin/gl.pdb"
+  "libgl.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
